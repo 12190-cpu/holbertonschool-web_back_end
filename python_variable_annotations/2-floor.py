97c7053eb9175af8771eed552c4cdfb919bdaae3
12190-cpu <12190@holbertonstudents.com>
2026-04-12 19:25:03 +0200

    Type Annotations

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module that provides a function to return the floor of a float."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a floating-point number.
+
+    Args:
+        n (float): The number to floor.
+
+    Returns:
+        int: The floor of n.
+    """
+    return math.floor(n)
