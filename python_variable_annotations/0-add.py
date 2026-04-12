a65d604cf00eeac7809a7ee48332be94621553c4
12190-cpu <12190@holbertonstudents.com>
2026-04-12 19:18:07 +0200

    Type Annotations

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module that provides a function to add two floating-point numbers."""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two floating-point numbers.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
