ea64ce70b3bf72312b6e4827b03f0d1949a9278c
12190-cpu <12190@holbertonstudents.com>
2026-04-21 12:20:56 +0200

    pagination

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """Module that provides a helper function for pagination."""
 
-
 from typing import Tuple
-
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Return a tuple containing the start and end indexes for pagination."""
