4cc48d762f426157a468be2d5c85f0a9c14927ec
12190-cpu <12190@holbertonstudents.com>
2026-04-21 12:14:08 +0200

    pagination

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr?bin?/env python3
+""" Modukle that provides a helper function for pagination """
+
+from typing import Tuple
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ Return a tuple comntaning the start index and end index for pagination. The page numbering is 1-indexed, meaning that page 1 starts at index 0. """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+start_index = (page - 1) * page_size
+end_index = start_index + page_size
+return (start_index, end_index)
