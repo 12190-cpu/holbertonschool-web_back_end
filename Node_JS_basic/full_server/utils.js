458337d31ad30c95c41b1f388b710379ada0413a
12190-cpu <12190@holbertonstudents.com>
2026-05-14 13:21:14 +0200

    NodeJS

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,34 @@
+import fs from 'fs';
+
+export default function readDatabase(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+
+      const lines = data
+        .trim()
+        .split('\n')
+        .filter((line) => line.trim() !== '');
+
+      const students = lines.slice(1);
+      const fields = {};
+
+      students.forEach((student) => {
+        const studentData = student.split(',');
+        const firstname = studentData[0];
+        const field = studentData[3];
+
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+
+        fields[field].push(firstname);
+      });
+
+      resolve(fields);
+    });
+  });
+}
